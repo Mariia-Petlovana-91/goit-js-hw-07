570b0b6e93b9352fcd2d65480b825eaeb0c61c0e
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2022-02-12 15:34:09 +0300

    create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galeriRef = document.querySelector('.gallery');
+galeriRef.insertAdjacentHTML('beforebegin', createGalleryItemsMarkap(galleryItems));
+galeriRef.addEventListener('click', onClickEvTarget);
+
+function onClickEvTarget(event) {
+
+  if (event.target.nodeName !== IMG) {
+    return;
+  }
+  else {
+
+  }
+}
+
+
+
+function createGalleryItemsMarkap(items) {
+   return items.map(item=> ` <div class="gallery__item">
+ <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`).join('');
+}
+
