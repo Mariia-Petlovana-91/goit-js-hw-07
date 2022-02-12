a441b8eb37311c701d5091d5bba7ccceca13ac8c
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2022-02-12 18:53:13 +0300

    work with modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galeriRef = document.querySelector('.gallery');
-galeriRef.insertAdjacentHTML('beforebegin', createGalleryItemsMarkap(galleryItems));
-galeriRef.addEventListener('click', onClickEvTarget);
-
-function onClickEvTarget(event) {
-
-  if (event.target.nodeName !== IMG) {
-    return;
-  }
-  else {
-    event.preventDefault();
-    instance.show();
-  }
-}
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
-console.log(instance);
-
-function createGalleryItemsMarkap(items) {
-   return items.map(item=> ` <div class="gallery__item">
- <a class="gallery__link" href=${item.original}>
+function itemMarkap(item) {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
-      src=${item.preview}
-      data-source=${item.original}
-      alt=${item.description}
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
     />
   </a>
-</div>`).join('');
+</div>`
 }
+
+const galleryItemsEl = galleryItems.map(itemMarkap).join('');
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', galleryItemsEl);
+galleryRef.addEventListener('click', onClickImg);
+
+ const instance = basicLightbox.create(`
+     <div class="modal">
+        <img src="">
+      </div>
+ `);
+instance.show();
+ 
+function onClickImg(event) {
+    if (event.target.tagName !== IMG) {
+        return;
+    }
+    else {
+      event.preventDefault();   
+     
+  }
+}
+
 
